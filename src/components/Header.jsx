0531d9da118e841e29b4b56e98c4328e229f7028
Tên Người Dùng Mới <email_moi@example.com>
2024-06-30 20:21:44 +0700

    update

---
+++ src/components/Header.jsx
@@ -0,0 +1,63 @@
+import logo from '.././assets/images/logo.png';
+import { HiHome, HiStar, HiPlus, HiDotsVertical } from "react-icons/hi";
+import { HiMagnifyingGlass, HiTv, HiPlayCircle } from "react-icons/hi2";
+import HeaderItem from './HeaderItem';
+import { useState } from 'react';
+
+function Header() {
+  const [toggle, setToggle] = useState(false)
+  const menu = [
+    {
+      name: 'HOME',
+      icon: HiHome
+    },
+    {
+      name: 'SEARCH',
+      icon: HiMagnifyingGlass
+    },
+    {
+      name: 'WATCH LIST',
+      icon: HiPlus
+    },
+    {
+      name: 'ORIGINALS',
+      icon: HiStar
+    },
+    {
+      name: 'MOVIE',
+      icon: HiPlayCircle
+    },
+    {
+      name: 'SERIES',
+      icon: HiTv
+    },
+  ];
+  return (
+    <div className="flex items-center  justify-between p-5">
+      <div className='flex gap-8 items-center'>
+        <img src={logo} className='w-[80px] md:w-[115px] object-cover' alt="logo" />
+        <div className='hidden md:flex gap-8'>
+          {menu.map((item, index) =>
+            (<HeaderItem key={index} name={""} Icon={item.icon} />)
+          )}
+        </div>
+        <div className='flex md:hidden gap-8'>
+          {menu.map((item, index) => index < 3 &&
+            (<HeaderItem key={index} name={""} Icon={item.icon} />)
+          )}
+          <div className='md:hidden' onClick={()=>setToggle(!toggle)}>
+            <HeaderItem name={''} Icon={HiDotsVertical} />
+            {toggle ? <div className='absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4'>
+              {menu.map((item, index) => index >= 3 &&
+                (<HeaderItem key={index} name={item.name} Icon={item.icon} />)
+              )}
+            </div> : null}
+          </div>
+        </div>
+      </div>
+      <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745" className='w-[40px] rounded-full' />
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Slider.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from 'react'
+import GlobalApi from '../services/GlobalApi'
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original"
+const screeWidth = window.innerWidth
+function Slider() {
+    const [movieList, setMovieList] = useState([])
+    const elementRef = useRef();
+
+    useEffect(() => {
+        getTrendingMovies()
+    }, [])
+
+    const getTrendingMovies = () => {
+        GlobalApi.getTrendingVideos.then(resp => {
+            console.log(resp.data.results)
+            setMovieList(resp.data.results)
+        })
+    }
+    const sliderRight = (element) => {
+        element.scrollLeft -= screeWidth - 110
+    }
+    const sliderLeft = (element) => {
+        element.scrollLeft += screeWidth - 110
+    }
+    return (
+        <div>
+            <FaChevronLeft className='hidden md:block text-white text-[30px] absolute mx-8 mt-[160px] cursor-pointer' onClick={(() => sliderRight(elementRef.current))} />
+            <FaChevronRight className='hidden md:block text-white text-[30px] absolute mx-8 mt-[160px] cursor-pointer right-0' onClick={(() => sliderLeft(elementRef.current))} />
+
+            <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-hide scroll-smooth' ref={elementRef}>
+                {movieList.map((item, index) => (
+                    <img key={index} src={IMAGE_BASE_URL + item.backdrop_path} className='min-w-full md:h-[420px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-100' />
+                )
+                )}
+            </div>
+        </div>
+
+    )
+}
+
+export default Slider
